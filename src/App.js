a247ce328a80d39be29053338eafde2124457390
bones <pdshvetsov@gmail.com>
2021-04-29 15:56:39 +0300

    homework3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class MyTodoList extends React.Component {
+  state = {
+      tasks: [
+          {
+          id: 1,
+          name: 'First task',
+          description: 'To do the 1st task',
+          completed: true
+          },
+          {
+            id: 12,
+            name: 'Second task',
+            description: 'To do the 2nd task',
+            completed: false
+          },
+          {
+            id: 123,
+            name: 'Third task',
+            description: 'To do the 3rd task',
+            completed: true
+          },
+          {
+            id: 1234,
+            name: 'Fourth task',
+            description: 'To do the 4th task',
+            completed: false
+          },
+          {
+            id: 12345,
+            name: 'Fifth task',
+            description: 'To do the 5th task',
+            completed: false
+          }
+      ]
+  }
+    render() {
+          return (
+                  <div>
+                    {this.state.tasks.map(task => <Task id = {task.id} name={task.name} description={task.description} completed={task.completed} />)}
+                  </div>
+                )
+      }
+      
+    }
 
+    const Task = ({id, name, description, completed}) => (
+    [
+    <div>{name}  {description} {completed ? "true" : "false"}</div>,
+    <button className="buttonstyle" onClick={console.log("Task " + id + " completed status " + completed)}>CLICK</button>
+    ]
+  )
+
+  const App = () => {
+    return <MyTodoList  />
+  }
+  
 export default App;
